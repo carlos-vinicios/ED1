a7b6ca2c5a6c49ef2511c773df33123539cb3afb
Carlos Vinicios Martins Rocha <vinicios_martins2011@hotmail.com>
2018-09-21 12:13:34 -0300

    Update TAD_FIla

--- TAD_fila/TAD_fila.c
+++ TAD_fila/TAD_fila.c
@@ -4,16 +4,16 @@
 Fila *criar(int qtd_max){
     Fila *fila = NULL;
 
-    if(qtd_max > 0){
-        fila = (Fila *) malloc(sizeof(Fila));
-        if(fila != NULL){
-            fila->dados = (void **) malloc(qtd_max * sizeof(void *));
-            if(fila->dados != NULL){
+    if(qtd_max > 0){ //a quantidade maxima precisa ser maior que 0
+        fila = (Fila *) malloc(sizeof(Fila)); //aloca a fila na memória
+        if(fila != NULL){ //verifica se alocou a fila
+            fila->dados = (void **) malloc(qtd_max * sizeof(void *)); //aloca o vetor de dados na memória
+            if(fila->dados != NULL){ //verifa o vetor de dados
                 fila->inicio = 0;
                 fila->fim = 0;
                 fila->qtd = 0;
-                fila->qtd_max = qtd_max;
-            }else{
+                fila->qtd_max = qtd_max; //define os valores iniciais da fila
+            }else{ //caso o vetor de dados não tenha sido alocado corretamente, limpa a fila da memória e define a mesma NULL
                 free(fila);
                 fila = NULL;
             }
@@ -24,11 +24,11 @@ Fila *criar(int qtd_max){
 }
 
 int enfileirar(Fila *fila, void *dado){
-    if(fila != NULL && fila->dados != NULL && fila->qtd < fila->qtd_max){
-        if(fila->fim == fila->qtd_max)
+    if(fila != NULL && fila->dados != NULL && fila->qtd < fila->qtd_max){ //verifica se a fila e vetor de dados estão na memória. Verifica se o vetor ainda possui espaços vazios
+        if(fila->fim == fila->qtd_max) //se a posição do fim da fila, equivaler ao seu tamanho máximo, retornando o seu valor para 0, caso verdadeiro
             fila->fim = fila->fim % fila->qtd_max;
 
-        fila->dados[fila->fim] = dado;
+        fila->dados[fila->fim] = dado; //realiza a inserção do dado no vetor
         fila->qtd++;
         fila->fim++;
         return 1;
@@ -38,12 +38,12 @@ int enfileirar(Fila *fila, void *dado){
 }
 
 void *desenfileirar(Fila *fila){
-    void *aux = NULL;
-    if(fila != NULL && fila->dados != NULL && fila->qtd > 0){
+    void *aux = NULL; //cria uma variável auxiliar para controle
+    if(fila != NULL && fila->dados != NULL && fila->qtd > 0){//verifica se a fila e o vetor de dados estão alocados na memória, e se existe algum elemento
         if(fila->inicio == fila->qtd_max)
             fila->inicio = fila->inicio % fila->qtd_max;
 
-        aux = fila->dados[fila->inicio];
+        aux = fila->dados[fila->inicio]; //realiza a chamada do elemento seguinte da fila
         fila->inicio++;
         fila->qtd--;
     }
@@ -51,7 +51,7 @@ void *desenfileirar(Fila *fila){
     return aux;
 }
 
-int vazia(Fila *fila){
+int vazia(Fila *fila){ //verifica se a fila está vázia
     if(fila != NULL && fila->dados != NULL && fila->qtd > 0)
         return 0;
     else
--- TAD_fila/TAD_fila.h
+++ TAD_fila/TAD_fila.h
@@ -1,20 +1,38 @@
 #ifndef TAD_fila
 #define TAD_fila
 
+/*Fila para controle do antendimento a coordenação*/
 typedef struct _Fila_{
-    void **dados;
-    int inicio;
-    int fim;
-    int qtd;
-    int qtd_max;
+    void **dados; //vetor de dados a ser armazenado
+    int inicio; //variavel de inicio do vetor
+    int fim; //variavel de fim do vetor
+    int qtd; //quantidade de elementos dentro do vetor
+    int qtd_max; //quantidade máxima de elementos dentro do vetor
 } Fila;
 
+/*Cria a fila de dados
+    @param qtd_max é quantidade máxima de elementos que o vetor de dados pode armazenar
+    @return um fila alocada, caso sucesso. NULL, caso erro
+*/
 Fila *criar(int qtd_max);
 
+/*Adiciona elementos a fila, respeitando a ordem de chegada de cada elemento
+    @param fila uma fila válida para registro e controle dos dados
+    @param dado elemento que deve ser adicionado ao fim da fila de elementos
+    @return 1 sucesso, 0 erro
+*/
 int enfileirar(Fila *fila, void *dado);
 
+/*Remove elementos da fila, respeitando a ordem de chegada de cada elemento
+    @param fila uma fila váida para controle dos dados
+    @return um dado correspondente a dada posição da fila
+*/
 void *desenfileirar(Fila *fila);
 
+/*Verifica se a fila está vázia
+    @param fila uma fila válida
+    @return 1 em caso seja verdade 0 em caso não esteja vázia
+*/
 int vazia(Fila *fila);
 
 #endif // TAD_fila
