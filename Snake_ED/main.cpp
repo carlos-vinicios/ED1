9326df625b8ac6cdcd7b41e24c5cc7c6ad9a8482
Carlos Vinicios Martins Rocha <vinicios_martins2011@hotmail.com>
2018-10-01 11:21:49 -0300

    Jogo Snake (Terminal)
    Windows e Linux

--- Snake_ED/main.cpp
+++ Snake_ED/main.cpp
@@ -1,57 +1,57 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <iostream>
 #include "TAD_fila.h"
 #include "TAD_pilha.h"
 #include "cenario.h"
 #include "snake.h"
 #include "fruta.h"
 
+//caso esteja no Linux, descomentar a proxima linha
 #define LINUX
 
-//https://www.vivaolinux.com.br/topico/C-C%2B%2B/kbhit()-no-linux
-
 #ifdef LINUX
-	#include <termios.h>
+    #include <sys/ioctl.h>
+    #include <termios.h>
     #include <unistd.h>
-    #include <fcntl.h>
 #else
-	#include <conio.h>
+    #include <conio.h>
     #include <Windows.h>
-    #include <iostream>
 #endif
 
 using namespace std;
 
 #ifdef LINUX
-int kbhit(void){
-  struct termios oldt, newt;
-  int ch;
-  int oldf;
+void enable_raw_mode()
+{
+    termios term;
+    tcgetattr(0, &term);
+    term.c_lflag &= ~(ICANON | ECHO); // Disable echo as well
+    tcsetattr(0, TCSANOW, &term);
+}
 
-  tcgetattr(STDIN_FILENO, &oldt);
-  newt = oldt;
-  newt.c_lflag &= ~(ICANON | ECHO);
-  tcsetattr(STDIN_FILENO, TCSANOW, &newt);
-  oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
-  fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
+void disable_raw_mode()
+{
+    termios term;
+    tcgetattr(0, &term);
+    term.c_lflag |= ICANON | ECHO;
+    tcsetattr(0, TCSANOW, &term);
+}
 
-  ch = getchar();
-
-  tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
-  fcntl(STDIN_FILENO, F_SETFL, oldf);
-
-  if(ch != EOF)
-  {
-    ungetc(ch, stdin);
-    return 1;
-  }
-
-  return 0;
+bool kbhit()
+{
+    int byteswaiting;
+    ioctl(0, FIONREAD, &byteswaiting);
+    return byteswaiting > 0;
 }
 #endif // LINUX
 
 void render(Fila *snake, char **cenario, Fruta *fruta, int lin, int col, int orien, int dire, int cen_tam, int pnts){
-    system("CLS");
+    #ifndef LINUX
+        system("CLS");
+    #else
+        system("clear");
+    #endif
     cout << "Pontos: " << pnts << '\n';
     start_cenario(cenario, cen_tam);
     draw_snake(snake, cenario, lin, col, orien, dire, cen_tam);
@@ -60,31 +60,42 @@ void render(Fila *snake, char **cenario, Fruta *fruta, int lin, int col, int ori
 }
 
 int main(){
+    #ifdef LINUX
+        enable_raw_mode();
+    #endif
     Fila *snake;
     Fruta *fruta;
     int cen_tam, pnts, lin, col, orien, dire, vel, exec=1;
     char **cenario;
-
-    //Definições iniciais do jogo
+    
+    //Definiï¿½ï¿½es iniciais do jogo
     lin = 3; //linha inicial
     col = 4; //coluna inicial
     orien = 180; //180 = horizontal 90 = vertical
     dire= 1; // dire > 0 eixo positivo | dire < 0 eixo negativo
     cen_tam = 30; //100x100
-    vel = 100; //tempo que passa no sleep
+    #ifndef LINUX
+    vel = 300; //tempo que passa no sleep
+    #else
+    vel = 1000000;
+    #endif
     pnts=0;
-    //fim das definições iniciais
+    //fim das definiï¿½ï¿½es iniciais
 
-    cenario = set_cenario(cen_tam); //aloca a memória para o cenário
-    snake = set_snake(cen_tam);//aloca a memória para a snake
+    cenario = set_cenario(cen_tam); //aloca a memï¿½ria para o cenï¿½rio
+    snake = set_snake(cen_tam);//aloca a memï¿½ria para a snake
     fruta = new_fruta(cen_tam, lin, col); //define uma posicao inicial para a fruta
 
     if(cenario != NULL && snake != NULL){
         while(exec){
             render(snake, cenario, fruta, lin, col, orien, dire, cen_tam, pnts);
-            #ifndef LINUX //código para windows
+            #ifndef LINUX //cï¿½digo para windows
             if (_kbhit()){
                 switch(_getch()){
+            #else //codigo para Linux
+            if (kbhit()){
+                switch(getchar()){
+            #endif
                     case 'w':
                         orien = 90;
                         dire = -1;
@@ -103,8 +114,8 @@ int main(){
                         break;
                 }
             }
-            #else //código para linux
-
+            #ifdef LINUX
+            tcflush(0, TCIFLUSH); // Clear stdin to prevent characters appearing on prompt
             #endif
             if(snake_eat(lin, col, fruta)){
                 eat_fruit(snake);
@@ -126,14 +137,20 @@ int main(){
             #ifndef LINUX
             Sleep(vel);
             #else
-
+            usleep(vel);
             #endif
         }
     }else{
         cout << "Erro na inicializacao do jogo, veja seu sistema operacional ou estado de memoria";
     }
-
+    #ifndef LINUX      
     system("CLS");
+    #else
+    system("clear");
+    #endif
     cout << "Game Over\n" << "Pontuacao total: " << pnts;
+    #ifdef LINUX
+    disable_raw_mode();
+    #endif
     return 0;
 }
